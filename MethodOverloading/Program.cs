df352a8ecdf5063d64b0a52793a24878d67d7077
kmadison382 <k.madison.382@gmail.com>
2024-06-16 15:55:58 -0500

    more fun with methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace MethodOverloading
+﻿using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool dollars)
+        {
+            string money;
+            if (a + b == 1)
+                {
+                    money = $"{a + b} dollar";
+                }
+            else
+                {
+                    money = $"{a + b} dollars";
+                }
+                return money;
+            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var two = Add(5, 13);
+            Console.WriteLine(two);
+            var decimals = Add(5.4, 8.7);
+            Console.WriteLine(decimals);
+            var howMuchMoney = Add(1, 0, true);
+            Console.WriteLine(howMuchMoney);
         }
     }
 }
